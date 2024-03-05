686ddd2236e41ecc88eccb137895ef501bec0a1c
Robson <robsonnfer@gmail.com>
2024-03-05 11:48:03 -0300

    aula 34

--- Aula034_dicionarios.py
+++ Aula034_dicionarios.py
@@ -33,7 +33,10 @@ paises1 = dict(br='Brasil', usa='Estados Unidos', par='Paraguai')
 print(f'Aqui temos o dicionário paises1: {paises}')
 print(f'Aqui temos o tipo do dicionário paises1: {type(paises)}')
 
+# ACESSANDO ELEMENTOS:
+
 print('------------------------------------------------------------------------------')
+# FORMA 1 - ACESSANDO PELA CHAVE:
 print('Acessando elementos pela chave:')
 # IMPORTANTE: Dicionários não são indexados
 # FORMA 1 - Acessando via chave:
@@ -43,5 +46,70 @@ print(f'Acessando o elemento Paraguai via chave com colchetes: {paises["par"]}')
 # Usando chave inexistente será gerado um erro.
 
 print('------------------------------------------------------------------------------')
+# FORMA 2 - ACESSANDO VIA GET (FORMA RECOMENDADA):
+print('Acessando elementos via get:')
+print(f'Acessando o elemento Brasil via get: {paises.get("br")}')
+print(f'Acessando o elemento Paraguai via get: {paises.get("par")}')
+print(f'Acessando um elemento que não existe via get: {paises.get("ru")}')
+# IMPORTANTE: Note que neste caso usar um elemento que não existe não retorna um erro, mas sim um tipo None
 
-# Parei a aula em 14:23 min
+"""
+Tipo None
+
+O tipo de dado None em Python representa a ausência de tipo ou conhecido também como tipo vazio.
+Resumindo, é a definição de um elemento sem tipo definido.
+
+OBS: O tipo None é SEMPRE especificado com a primeira letra maiúscula.
+
+Quando devemos utilizar o tipo None?
+- Quando quisermos criar uma variável e inicializá-la sem tipo antes de receber um valor final defindo um tipo.
+
+IMPORTANTE: CONCLUINDO ENTÃO, E MELHOR BUSCAR O ITEM PELO GET DO QUE PELA CHAVE, POIS COM GET SE DAR ERRO A APLICAÇÃO NÃO PARA.
+    PELA CHAVE PODEMOS TRATAR O ERRO, MAS COM GET ELIMINA-SE A ETAPA DE TRATATIVA DESSE ERRO!
+
+OBS: O tipo None será SEMPRE considerado como False!
+"""
+
+print('Tipo None')
+# Exemplificando:
+
+numeros = None
+print(f'Mostrando a variável numeros: {numeros}')
+print(f'Mostrando o tipo da variável numeros: {type(numeros)}')
+
+print('Variável do tipo None mudando de tipo.')
+
+numeros = 1.44, 1.34, 5.67
+print(f'Mostrando a variável numeros depois de receber dados: {numeros}')
+print(f'Mostrando o tipo da variável numeros: {type(numeros)}')
+
+# Exemplo de busca usando o get com if/else e evitando erro:
+print('Exemplo de busca usando o get com if/else e evitando erro:')
+
+Russia = paises.get('ru')
+if Russia:
+    print('Encontrei o País')
+else:
+    print('Não encontrei o País')
+
+# Exemplo de busca usando get sem if/else:
+print('Usando o mesmo código acima, mas sem o if/else:')
+Dinamarca = paises.get('din', 'Não encontrado')
+print(Dinamarca)
+
+# IMPORTANTE: O tipo None sempre será falso:
+if None:
+    print('Sim')
+else:
+    print('Não')
+
+print('------------------------------------------------------------------------------')
+# VERIFICANDO SE DETERMINADO ELEMENTO ESTÁ DENTRO DO DICIONÁRIO POR MEIO DA CHAVE:
+print('br' in paises)
+print('ru' in paises)
+print('Estados Unidos' in paises)
+# Temos a chave 'br' por isso o resultado será True, mas não temos 'ru' nem 'Estados Unidos' como chaves, por isso resulta em False.
+# OBS: 'Estados Unidos' é um elemento e não uma chave!
+if 'mex' in paises:
+    mexico = paises['mex']
+# Se 'mex' existir em paises, a variavel mexico recebe 
