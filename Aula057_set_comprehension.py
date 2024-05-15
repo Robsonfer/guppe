78fb9e9922e5b95ee35d98f8b3cbe3565a2cbdd3
Robson <robsonnfer@gmail.com>
2024-05-15 16:03:44 -0300

    Aula057

--- Aula057_set_comprehension.py
+++ Aula057_set_comprehension.py
@@ -1,5 +1,23 @@
 """
 Set Comprehension
 
-
+lista = [1, 2, 3, 4, 5]
+set = {1, 2, 3, 4, 5}
 """
+
+# Exemplo 1:
+numeros = {num for num in range(1, 7)}
+print(numeros)
+print(type(numeros))
+
+# Exemplo 2:
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+
+# DESAFIO: Faça uma alteração na estrutura acima para gerar um dicionário ao invés de Set:
+num = {x: x ** 2 for x in range(10)}
+print(num)
+
+# Exemplo 3 com strings:
+letras = {letra for letra in 'Geek University'}
+print(letras)
