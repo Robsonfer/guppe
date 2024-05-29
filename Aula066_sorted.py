b3038ae7b25f7641ef37086234ff3bffbf6918af
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-05-28 22:40:21 -0300

    Aula066

---
+++ Aula066_sorted.py
@@ -0,0 +1,17 @@
+"""
+Sorted
+
+OBS: Não confunda com a função sort(), pois o sort() só funciona em listas.
+
+Podemos utilizar o sorted() com qualquer iterável. Como o próprio nome diz, sorted() é utilizado para ordenar
+"""
+
+# Relembrando o sort():
+lista = [4, 7, 3, 8, 1, 5, 9, 2, 0]
+lista.sort()
+print(lista)
+
+# Exemplo de sorted() com lista:
+numeros = [6, 1, 8, 2]
+print(numeros)
+print(sorted(numeros))
