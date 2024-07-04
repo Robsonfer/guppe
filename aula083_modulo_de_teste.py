be431b651828800327c1b329086a16bee285836b
Robson <robsonnfer@gmail.com>
2024-07-04 15:44:14 -0300

    aula083

--- aula083_modulo_de_teste.py
+++ aula083_modulo_de_teste.py
@@ -14,3 +14,24 @@ def gera_aleatorio():
 # -------- Gerando número aleatório de 0 a 100 com uniform() --------
 def gera_aleatorio_uniform():
     print(f'Este é um número aleatório entre 0 e 100 gerado com uniform: {round(uniform(1, 101))}')
+
+
+# -------- Somando somente os números ímpares de uma lista --------
+def soma_impares(numeros):
+    total = 0
+    for num in numeros:
+        if num % 2 != 0:
+            total += num
+    return total
+
+
+cidades = [('Berlin', 29), ('Cairo', 36), ('Buenos Aires', 19), ('Los Angeles', 26), ('Tokio', 27), ('Nova York', 28), ('Londres', 22)]
+
+# Converter de graus Celsius para Farenheight:
+# Fórmula: f = 9/5 * c  + 32
+c_to_f = lambda dado: (dado[0], (9/5) * dado[1] + 32)
+
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+tupla = (1, 3, 5, 7, 9, 11, 13, 15)
--- aula083_modulos_customizados.py
+++ aula083_modulos_customizados.py
@@ -5,22 +5,37 @@ Como módulos Python nada mais são do que arquivos Python, então TODOS os arqu
 módulos Python prontos para serem utilizados.
 """
 
-# Importando uma função específica do nosso módulo!
+# Importando uma função específica do nosso módulo. A função importada é a gera_aleatorio():
 from aula083_modulo_de_teste import gera_aleatorio
 
 gera_aleatorio()
 
 
-# Importando nosso módulo inteiro:
+# Importando um módulo inteiro:
 import aula083_modulo_de_teste as mt
 
 # from aula083_modulo_de_teste import *
-
 mt.gera_aleatorio_uniform()
 
-"""
-OBSERVAÇÃO IMPORTANTE: Na aula o professor usa uma aula antiga para importar uma função. Eu criei um arquivo novo
-para fazer o teste!
 
-OBS: importei usando alias, mas tbm poderia importar usando *. Isso evitaria ficar chamando a função com o alias!
 """
+OBSERVAÇÃO IMPORTANTE: Na aula o professor usa uma aula antiga para importar uma função, mas tive problemas com o
+print das funções e como não quero mudar a estrutura das aulas para que possa voltar e revisar o conteúdo, então eu
+criei um arquivo novo completamente do zero (aula083_modulo_de_teste.py) para fazer esta aula e entender como
+funciona todo o processo do começo ao fim!
+
+OBSERVAÇÃO IMPORTANTE: Neste segundo exemplo eu importei usando alias, mas tbm poderia importar usando *. Importar
+usando o * deixaria o código mais limpo porque evitaria ficar chamando o módulo antes da função
+(mt.gera_aleatorio_uniform()), visto que nós estamos importando o módulo inteiro.
+"""
+
+
+# Acessando e imprimindo variáveis de outro módulo:
+print(f'Esta lista vem de outro módulo: {mt.lista}')
+print(f'Esta tupla vem de outro módulo: {mt.tupla}')
+
+# Usando uma função e uma variável importadas de outro módulo simultaneamente:
+print(f'Essa é a soma dos números ímpares da lista do outro módulo: {mt.soma_impares(mt.lista)}')
+
+# Peguei mais funções de outros módulos e colei no módulo de teste:
+print(f'Mais um caso de função e variável de outro módulo: {list(map(mt.c_to_f, mt.cidades))}')
