e3352acbaa31e644878a0370f9a7c15fdf0fffa7
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-07-09 17:22:09 -0300

    aula084

--- aula084_modulos_externos.py
+++ aula084_modulos_externos.py
@@ -1,15 +1,18 @@
 """
 Módulos Externos
 
-Utilizamos o gerenciador de pacotes python chamado pip - Python Installer Package
+Utilizamos o gerenciador de pacotes python chamado pip - Python Package Index
 
 Como eu consigo saber quais pacotes existem para Python?
     Você pode acessar o site: https://pypi.org
 
 colorama: É utilizado para permitir impressão de textos coloridos no terminal.
+
+Instalando o módulo: pip install colorama
 """
 
 # Utilizando o pacote externo colorama após instalado:
+"""
 from colorama import init
 init()
 
@@ -28,3 +31,15 @@ print(Back.LIGHTWHITE_EX + 'Colocando um background branco!')
 print(Style.DIM + 'and in dim text')
 print(Style.RESET_ALL + 'Resetando tudo')
 print('Agora, tudo de volta ao normal')
+"""
+
+# Gerando PDF com o pacote fpdf:
+"""
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('helvetica', size=12)
+pdf.cell(text="Geek University")
+pdf.output("geekuniversity.pdf")
+"""
