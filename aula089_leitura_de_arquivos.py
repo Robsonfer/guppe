8b5b65e793a98616aa454c021d90927048d8fc18
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-07-10 20:17:01 -0300

    aula089

---
+++ aula089_leitura_de_arquivos.py
@@ -0,0 +1,10 @@
+"""
+Leitura de Arquivos
+
+Para ler o conteúdo de um arquivo em Python, utilizamos a função open(), que literalmente sigfica "abrir".
+
+open() = Na forma mais simples de utilização, nós passamos apenas um parâmetro de entrada, que neste caso é o caminho
+para o arquivo a ser lido. Essa função retorna um _io.TextIOWraper e é com ele que trabalhamos então.
+
+
+"""
