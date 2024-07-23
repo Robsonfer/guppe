b0c72c3ddcf81bf78b113847bc762cf635c8091a
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-07-22 21:43:43 -0300

    aula096 meio

--- aula096_sistema_de_arquivos_manipulacao.py
+++ aula096_sistema_de_arquivos_manipulacao.py
@@ -34,14 +34,40 @@ open('arquivo_teste2.txt', 'w').close()
 # Forma 3:
 with open('arquivo_teste3.txt', 'w') as arquivo:
     pass
-# Esse pass serve somente para que seja obedecida a indentação, pois ':' abre um bloco de código. Com o with o arquivo fecha ao final do bloco
+# Esse pass serve somente para que seja obedecida a indentação, pois ':' abre um bloco de código.
+Com o with o arquivo fecha ao final do bloco.
 
+# Melhor forma de criar arquivos, mas que não funciona no Windows:
+os.mknod('arquivo-teste4.txt')
+os.mknod('C:/Users/robsonfer/OneDrive - Armco do Brasil S.A/Perfil/Desktop/ROBSON/UDEMY/guppe/geek/university.txt')
+
+OBSERVAÇÃO IMPORTANTE: Este comando mknod() NÃO FUNCIONA NO WINDOWS!
+Para Windows use sempre uma das formas entre 1 e 3 mencionados acima
+
+OBS: Criando um arquivo via mknod(), se o arquivo já existir, teremos o erro FileExistisError.
+Mas sabemos como tratar erros.
+
+
+# Criando diretórios:
+
+# Path Relativo
+try:
+    os.mkdir('templates')
+except FileExistsError:
+    print('ATENÇÃO, ESTE DIRETÓRIO JÁ EXISTE, TENTE OUTRO NOME OU CAMINHO!')
+# Esse diretório será criado onde o Python está a ser executado. E caso já exista, teremos um FileExistsError.
+# Mas já tratamos com try/except o exemplo acima.
+
+# Path Absoluto:
+try:
+    os.mkdir('C:/Users/robso/PycharmProjects/guppe/geek/templates')
+except FileExistsError:
+    print('ATENÇÃO, ESTE DIRETÓRIO JÁ EXISTE, TENTE OUTRO NOME OU CAMINHO!')
+
+# OBS: Se não tivermos permissão para criar o diretório, teremos um PermissionError
 
 
 """
 
 import os
 
-# Melhor forma de criar arquivos:
-# os.mknod('arquivo-teste4.txt')
-# os.mknod('C:/Users/robsonfer/OneDrive - Armco do Brasil S.A/Perfil/Desktop/ROBSON/UDEMY/guppe/geek/university.txt')
