1cfdc4efe657744e98ac61c88fe402871d9e09bf
Robson <robsonnfer@gmail.com>
2024-07-24 15:16:12 -0300

    aula096 meio

--- aula096_sistema_de_arquivos_manipulacao.py
+++ aula096_sistema_de_arquivos_manipulacao.py
@@ -37,6 +37,8 @@ with open('arquivo_teste3.txt', 'w') as arquivo:
 # Esse pass serve somente para que seja obedecida a indentação, pois ':' abre um bloco de código.
 Com o with o arquivo fecha ao final do bloco.
 
+
+
 # Melhor forma de criar arquivos, mas que não funciona no Windows:
 os.mknod('arquivo-teste4.txt')
 os.mknod('C:/Users/robsonfer/OneDrive - Armco do Brasil S.A/Perfil/Desktop/ROBSON/UDEMY/guppe/geek/university.txt')
@@ -48,7 +50,8 @@ OBS: Criando um arquivo via mknod(), se o arquivo já existir, teremos o erro Fi
 Mas sabemos como tratar erros.
 
 
-# Criando diretórios:
+
+# Criando diretórios Únicos:
 
 # Path Relativo
 try:
@@ -65,6 +68,55 @@ except FileExistsError:
     print('ATENÇÃO, ESTE DIRETÓRIO JÁ EXISTE, TENTE OUTRO NOME OU CAMINHO!')
 
 # OBS: Se não tivermos permissão para criar o diretório, teremos um PermissionError
+
+
+
+# Criando diretórios múltiplos (árvore de diretórios):
+os.makedirs('templates/geek/university')
+
+# OBS: Caso todo o diretório criado já exista, teremos um FileExistisError
+# Caso você queira criar mais uma pasta nesete diretório, basta acrescentar a pasta no final e não dará erro.
+
+
+
+# Tratando erros na criação de diretórios de maneira direta:
+
+# Basta colocar um parâmetro a mais conforme abaixo e mesmo que já exista o diretório, não apresentará erro:
+os.makedirs('templates2/novo2/outro2', exist_ok=True)
+
+
+
+# Renomeando arquivos e diretórios:
+
+# Diretórios:
+os.rename('templates2', 'geek2')
+
+# Renomeando vários arquivos e diretórios ao mesmo tempo:
+os.rename('geek2/novo2', 'geek2/novo')
+
+# OBS: Se o diretório não existe, haverá um FileNotFoundError
+# OBS: Se o diretório que queremos renomear não estiver vazio, teremos um OSError
+
+# Arquivos:
+os.rename('geek2/novo/outro2/arquivo_teste2.txt', 'geek2/novo/outro2/arquivo_teste3.txt')
+# É importante colocar o Path completo.
+
+
+
+# Deletando arquivos:
+
+os.remove('geek2/novo/outro2/arquivo_teste4.txt')
+
+OBSERVAÇÕES IMPORTANTES:
+
+1 - Se você estiver no Windows e o arquivo que você for deletar estiver em uso, você terá um erro.
+2 - Se o arquivo não exista, teremos o FileNotFoundError.
+3 - Se você informar um diretório ao invés de um arquivo, teremos um IsADirectoryError.
+    os.remove() é somente para arquivos e não diretórios.
+
+ATENÇÃO: TOMEM CUIDADO COM OS COMANDOS DE DELEÇÃO. AO DELETAR UM ARQUIVO OU DIRETÓRIO.
+OS ARQUIVOS DELETADOS VIA OS NÃO VÃO PARA A LIXEIRA, ELES SOMEM.
+
 
 
 """
