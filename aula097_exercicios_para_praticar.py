f4a729a2736a28f4975a33b8ede5e0808613fae5
Robson <robsonnfer@gmail.com>
2024-07-26 15:09:43 -0300

    aula097 exercicios

---
+++ aula097_exercicios_para_praticar.py
@@ -0,0 +1,22 @@
+"""
+Exercício 1:
+    Crie um programa que:
+        a) Crie/abra um arquivo texto de nome "arq.txt";
+        b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o usuário entre com o caractere "0";
+        c) Feche o arquivo;
+        d) Abra, leia o arquivo e escreva na tela todos os caracteres armazenados.
+"""
+
+
+
+"""
+Exercício 2:
+    Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas letras são vogais e quantas são consoantes.
+"""
+
+
+
+"""
+Exercício 3:
+    Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas linhas este arquivo possui.
+"""
