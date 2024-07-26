ef5ddaf1d6517f4dc87fa7565706cd9ec1f50b14
Robson <robsonnfer@gmail.com>
2024-07-26 15:30:28 -0300

    aula097 atividade 1

--- aula097_exercicios_para_praticar.py
+++ aula097_exercicios_para_praticar.py
@@ -2,10 +2,23 @@
 Exercício 1:
     Crie um programa que:
         a) Crie/abra um arquivo texto de nome "arq.txt";
-        b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o usuário entre com o caractere "0";
+        b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o usuário entre com o caractere "O";
         c) Feche o arquivo;
         d) Abra, leia o arquivo e escreva na tela todos os caracteres armazenados.
 """
+
+import os
+
+with open('arq.txt', 'a') as arquivo:
+    while True:
+        caractere = input('Digite o que quiser ou "O" para sair: ')
+        if caractere != 'O':
+            arquivo.write(caractere + '\n')
+        else:
+            break
+
+with open('arq.txt') as arquivo:
+    print(arquivo.read())
 
 
 
