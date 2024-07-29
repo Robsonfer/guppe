14f3b906e6deb59415f9c8dbc6d261aeff40f4be
Robson <robsonnfer@gmail.com>
2024-07-29 13:48:47 -0300

    aula100

---
+++ aula100_iteradores_e_iteraveis.py
@@ -0,0 +1,11 @@
+"""
+Entendendo Iterators e Iterables
+
+Iterator:
+    - Objeto que pode ser iterado;
+    - Objeto que retorna um dado, sendo um elemento por vez quando uma função next() é chamada.
+
+Iterable:
+    - Objeto que irá retornar um iterador quando a função iter() for chamada.
+"""
+
