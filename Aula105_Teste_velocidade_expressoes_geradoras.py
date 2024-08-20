e274098e1fbab381f3b412e15ffac3b35c60b826
Robson <robsonnfer@gmail.com>
2024-08-20 09:43:34 -0300

    aula105 final

--- Aula105_Teste_velocidade_expressoes_geradoras.py
+++ Aula105_Teste_velocidade_expressoes_geradoras.py
@@ -34,5 +34,21 @@ print(next(ge2))
 # Os dois são executados de formas diferentes.
 """
 
-# Podemos usar funções integradas do Python, por exemplo:
-print(sum(num for num in range(1, 10)))
+# Realizando o teste de velocidade:
+import time
+
+# Generator Expression:
+
+gen_inicio = time.time()
+print(sum(num for num in range(100000000))) # 100 milhões
+gen_tempo = time.time() - gen_inicio
+
+
+# List Comprehension:
+
+list_inicio = time.time()
+print(sum([num for num in range(100000000)])) # 100 milhões
+list_tempo = time.time() - list_inicio
+
+print(f'O Generator Expression levou {gen_tempo} segundos.')
+print(f'O List Comprehension levou {list_tempo} segundos.')
