dfa14db616139a45d87e201d5a02326a237f6455
Robson <robsonnfer@gmail.com>
2024-08-23 10:33:57 -0300

    aula110 fim

--- aula110_decoradores_diferentes_assinaturas.py
+++ aula110_decoradores_diferentes_assinaturas.py
@@ -59,3 +59,52 @@ print(saudacao('Angelina'))
 print(ordenar('Hot Roll', 'Molho Tarê'))
 
 print(ordenar('picanha', 'molho de pimenta'))
+
+# A assinatura de uma função é representada pelo seu retorno, nome e parâmetros de entrada.
+# A questão de diferentes assinaturas fica à cargo dos parâmetros *args e **kwargs que permitem que utilizemos quantos parâmetros quisermos.
+
+@gritar
+def lol():
+    return 'lol'
+
+print(lol())
+
+# Por exemplo nesta última função que não tem parâmetro nenhum, mas funciona do mesmo jeito quando decorada com @gritar.
+
+# OBS: Vale lembrar também que podemos utilizar parâmetros nomeados:
+
+print(ordenar(acompanhamento='batata frita', principal='picanha'))
+
+
+
+# Decorator com argumentos:
+
+def verifica_primeiro_argumento(valor):
+    def interna(funcao):
+        def outra(*args, **kwargs):
+            if args and args[0] != valor:
+                return f'Valor incorreto! O primeiro argumento precisa ser igual a {valor}.'
+            return funcao(*args, **kwargs)
+        return outra
+    return interna
+
+
+@verifica_primeiro_argumento('pizza')
+def comida_favorita(*args):
+    print(args)
+
+
+@verifica_primeiro_argumento(10)
+def soma_dez(num1, num2):
+    return num1 + num2
+
+
+# Testando:
+
+print(soma_dez(10, 12)) # 22
+
+print(soma_dez(1, 21)) # Deveria ser 22 tbm, mas o primeiro argumento não é 10, retorna o aviso.
+
+print(comida_favorita('pizza', 'churrasco')) # pizza, churrasco
+
+print(comida_favorita('sanduíche', 'pizza')) # Deveria ser sanduíche e pizza, mas como o primeiro argumento não é pizza, retorna o aviso.
