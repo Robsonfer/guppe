934d59998b04b9c21625e16d5ff081de5afc8bc5
Robson <robsonnfer@gmail.com>
2024-09-02 15:10:48 -0300

    aula117

---
+++ aula117_atributos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+POO - ATRIBUTOS
+
+Atributos: Representam as características do objeto, ou seja, pelos atributos nós conseguimos representar
+computacionalmente os estados de um objeto.
+
+Em Python dividimos os atributos em três grupos:
+    - Atributos de instância;
+    - Atributos de classe;
+    - Atributos dinâmicos.
+
+ATRIBUTO DE INSTÂNCIA: São atributos declarados dentro do método construtor.
+
+OBS: O Método Construtor é um método especial utilizado para a construção do objeto
+
+"""
+
