aea493795a8d24efe3a6c25021e859f3dfe90624
Robson <robsonnfer@gmail.com>
2024-10-07 16:10:56 -0300

    aula137 inicio

---
+++ aula137_trabalhando_com_JSON_pickle.py
@@ -0,0 +1,34 @@
+"""
+Aula 137 - Trabalhando com JSON e Pickle
+
+JSON -> Javascript Object Notation
+
+API -> São meios de comunicação entre os serviços oferecidos por empresas como o Twitter, Facebook, YouTube
+    dentre várias outras e teceiros (nós desenvolvedores).
+
+Exemplo:
+
+ret = json.dumps(['produto', {'Playstation 4': ('2TB', 'Novo', '220V', 2340)}])
+
+print(type(ret))
+
+print(ret)
+
+
+"""
+
+import json
+
+
+class Gato:
+    def __init__(self, nome, raca) -> None:
+        self.__nome = nome
+        self.__raca = raca
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def raca(self):
+        return self.raca
