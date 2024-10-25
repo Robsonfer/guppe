009cb6efbefe397fab832a2d9ae9dea4cdf82f65
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-10-24 21:32:29 -0300

    aula146 fim

--- aula146_assertions.py
+++ aula146_assertions.py
@@ -9,14 +9,34 @@ Se a expressão for verdadeira, o 'assert' retorna None e caso seja falsa, levan
 OBS: Nós podemos especificar ocasionalmente um segundo argumento ou mesmo uma mensagem de erro personalizada.
 
 OBS: A palavra 'assert' pode ser utilizada em qualquer função ou código. Não precisa ser exclusivamente nos testes.
+
+ALERTA: Cuidado ao utilizar o 'assert'.
+    Se um programa Python for executado com o parâmetro -O, nenhum assertion será validado.
 """
 
 
-def soma_numeros_positovos(a, b):
+def soma_numeros_positivos(a, b):
     assert a > 0 and b > 0, 'Atenção! Ambos os números precisam ser positivos!'
     return a + b
 
 
-ret = soma_numeros_positovos(2, -2)
+ret = soma_numeros_positivos(2, 2)  # Ok
+# ret = soma_numeros_positivos(2, -2) # AssertionError
 
 print(ret)
+
+
+def comer_fast_food(comida):
+    assert comida in [
+        'pizza',
+        'sorvete',
+        'doce',
+        'batata frita',
+        'cachorro quente'
+    ], 'A comida precisa ser fast food'
+    return f'Eu estou comendo {comida}'
+
+
+comida = input('Informe a comida: ')
+
+print(comer_fast_food(comida))
