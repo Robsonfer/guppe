c51423aae3651a434f368b1b4f9af7910f53edf0
Robson <robsonnfer@gmail.com>
2024-11-11 15:26:59 -0300

    aula153 teoria

---
+++ aula153_alocacao_gerenciamento_memoria.py
@@ -0,0 +1,9 @@
+"""
+Aula 153 - Alocação e Gerenciamento de Memória em Python
+
+Aula teórica
+
+=============================================================================
+VER ARQUIVO 02-alocacao-e-gerencia-de-memoria-em-python.pdf NESTE REPOSITÓRIO
+=============================================================================
+"""
