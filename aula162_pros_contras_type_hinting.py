722065bc0f3ee47958c19dd9ac6a8e5a48c4459a
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-11-20 21:09:26 -0300

    aula 162 finalizada!

---
+++ aula162_pros_contras_type_hinting.py
@@ -0,0 +1,16 @@
+"""
+Aula 162 - Prós e contras do uso de Type Hinting (aula teórica)
+
+VANTAGENS:
+    1 - Facilita encontrar erros antes de executar o código;
+    2 - Melhora a documentação do seu código;
+    3 - Melhora a funcionalidade das IDEs para autocomplete e sugestões;
+
+DESVANTAGENS:
+    1 - Demora para acostumar a usar o type hinting;
+    2 - Apesar de ser implementado na versão 3.5 do Python, ele só ficou completo na versão 3.7;
+    3 - Traz uma ligeira queda de desempenho dos programas python;
+
+OBS:
+    É muito importante usar este tipo de recurso considerado conhecimento avançado de Python.
+"""
