85a5b0ff31706a18d2afcff616e0332a3036a035
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-12-02 22:09:45 -0300

    aula 168 inicio

---
+++ aula168_operador_walrus.py
@@ -0,0 +1,14 @@
+"""
+Aula 168 - Operador Walrus
+
+O Operador Walrus permite fazer a atribuição e o retorno de valor numa única expressão:
+
+variável := expressão
+"""
+
+nome = 'Geek University'
+print(nome)
+# No caso anterior a gente primeiro atribui a variável e somente depois imprime.
+
+print(nome1 := 'Robson Ferreira')
+# Neste caso fazemos ao mesmo tempo, declarar a variável, atribuir o valor da variável e imprimir esta variável
