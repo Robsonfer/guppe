9ff32b26af9afcce6f4813b48e51fb9cfe158273
Robson <76750320+Robsonfer@users.noreply.github.com>
2024-12-25 15:56:01 -0300

    projeto 3 banco

---
+++ Projeto_3_Banco/teste.py
@@ -0,0 +1,8 @@
+from models.cliente import Cliente
+from models.conta import Conta
+
+
+felicity = Cliente('Felicity Jones', 'felicity@gmail.com', '123.456.789-90', '02/09/1987')
+angelina = Cliente('Angelina Jolie', 'angelina@gmail.com', '987.654.321-12', '08/07/1978')
+
+print(felicity)
